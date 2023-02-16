26b431624ef6213b78c2674087aa896723e9cc8b
Станислав Андреевич Стативко <StativkoSA@parus31.local>
2023-02-16 12:04:52 +0300

    fix webpack dev plugin

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -18,12 +18,12 @@ export function buildPlugins(
     new webpack.DefinePlugin({
       __IS_DEV__: isDev,
     }),
-    new BundleAnalyzerPlugin({ openAnalyzer: false }),
   ];
 
   if (isDev) {
     plugins.push(new ReactRefreshWebpackPlugin());
     plugins.push(new webpack.HotModuleReplacementPlugin());
+    plugins.push(new BundleAnalyzerPlugin({ openAnalyzer: false }));
   }
 
   return plugins;
