f5b3ed8869a4e5d7026bfc18b69d2d6c058af9bc
Станислав Андреевич Стативко <StativkoSA@parus31.local>
2023-02-27 09:22:54 +0300

    add stories for Input and LoginForm

---
+++ src/features/AuthByUserName/ui/LoginForm/LoginFrom.stories.tsx
@@ -0,0 +1,25 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+
+import { LoginForm } from './LoginForm';
+
+export default {
+  title: 'features/LoginForm',
+  component: LoginForm,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof LoginForm>;
+
+const Template: ComponentStory<typeof LoginForm> = (args) => <LoginForm {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+};
+// Primary.decorators = [ThemeDecorator(Theme.LIGHT)];
+
+export const Dark = Template.bind({});
+Dark.args = {
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
---
+++ src/shared/ui/Input/Input.stories.tsx
@@ -0,0 +1,29 @@
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+import { ThemeDecorator } from 'shared/config/storybook/ThemeDecorator/ThemeDecorator';
+import { Theme } from 'app/providers/ThemeProvider';
+
+import { Input } from './Input';
+
+export default {
+  title: 'shared/Input',
+  component: Input,
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (args) => <Input {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+  placeholder: 'Type text',
+  value: 'Text',
+};
+// Primary.decorators = [ThemeDecorator(Theme.LIGHT)];
+
+export const Dark = Template.bind({});
+Dark.args = {
+  placeholder: 'Type text',
+  value: 'Text',
+};
+Dark.decorators = [ThemeDecorator(Theme.DARK)];
